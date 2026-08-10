bc27a668a965da9a6d17192bf6e6a9e771b749b0
dv313 <deardavidvidal@gmail.com>
2026-08-10 15:14:10 -0400

    Refactor DFT helper function and fix complex number usage

--- fft.py
+++ fft.py
@@ -1,10 +1,10 @@
 import numpy as np
 def W(n):
-    return np.exp(j*2*np.pi/n)
+    return np.exp(1j*2*np.pi/n)
 def add_zeros(x):
     return x.extend([0]*(len(x)-2**len(x).bit_length()))
 
-def dft_helper(x, y):
+def dft(x):
     if len(x) == 1:
         return x
     if set(x) == {0}:
@@ -13,7 +13,6 @@ def dft_helper(x, y):
     x_1 = x[::2]
     x_2 = x[1::2]
 
-    return [dft_helper(x_1)[k%(n//2)]  + \
-        W(len(x)//2)**(-k)*dft_helper(x_2)[k%(n//2)] \
+    return [dft(x_1)[k%(n//2)]  + \
+        W(len(x)//2)**(-k)*dft(x_2)[k%(n//2)] \
             for k in range(n)]
-    #return merge(dft(x[::2]) , dft(x[1::2])
